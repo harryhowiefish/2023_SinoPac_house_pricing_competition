fa3406d4a2115951953ae01b78fc0f6db5a8608a
harryhowiefish <>
2024-05-14 05:45:03 +0800

    docs: :memo: readme fix, code reformat

--- data_prep.py
+++ data_prep.py
@@ -1,10 +1,13 @@
-from external_data_cleaning import *
-from feature_engineering import *
+from external_data_cleaning import ext_cleaning
+from feature_engineering import make_all_feature
+
+
 def main():
     ext_cleaning('./data/external_data')
-    train,test = make_all_feature('./data')
-    train.to_csv('final_train_processed.csv',index=False)
-    test.to_csv('final_test_processed.csv',index=False)
+    train, test = make_all_feature('./data')
+    train.to_csv('final_train_processed.csv', index=False)
+    test.to_csv('final_test_processed.csv', index=False)
+
 
 if __name__ == '__main__':
     main()
--- training_and_inference.py
+++ training_and_inference.py
@@ -2,14 +2,9 @@ import pandas as pd
 import numpy as np
 import catboost as cb
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
-from scipy.spatial.distance import cdist
-import pickle
-from sklearn.metrics import mean_absolute_percentage_error
-from sklearn.cluster import KMeans
-from kmodes import kmodes
 
-CAT_COL = ['縣市','new_town','鄉鎮市區','使用分區','主要建材','建物型態','主要用途']
+CAT_COL = ['縣市', 'new_town', '鄉鎮市區', '使用分區', '主要建材', '建物型態', '主要用途']
+
 
 class mapeMetric(object):
     def get_final_error(self, error, weight):
@@ -30,41 +25,52 @@ class mapeMetric(object):
         for i in range(len(approx)):
             w = 1.0 if weight is None else weight[i]
             weight_sum += w
-            error_sum += w * abs((np.e**approx[i] - np.e**target[i])/np.e**target[i])
+            error_sum += w * \
+                abs((np.e**approx[i] - np.e**target[i])/np.e**target[i])
 
         return error_sum, weight_sum
 
-def training(model:cb.core.CatBoostRegressor,path:str):
+
+def training(model: cb.core.CatBoostRegressor):
     train = pd.read_csv('final_train_processed.csv')
     for col in CAT_COL:
-        train[col].fillna('',inplace=True)
+        train[col].fillna('', inplace=True)
     target = train['單價']
-    train.drop(['ID','路名', '橫坐標', '縱坐標', '備註','單價'],axis=1,inplace=True)
-    X_train, X_val, y_train, y_val = train_test_split(train, target, test_size = 0.2, random_state=0)
-    train_dataset = cb.Pool(X_train, np.log(y_train),cat_features=CAT_COL) 
-    val_dataset = cb.Pool(X_val, np.log(y_val),cat_features=CAT_COL) 
-    summary = model.select_features(train_dataset,eval_set=val_dataset,steps=5,features_for_select=train_dataset.get_feature_names(),num_features_to_select=422)
+    train.drop(['ID', '路名', '橫坐標', '縱坐標', '備註', '單價'], axis=1, inplace=True)
+    X_train, X_val, y_train, y_val = train_test_split(
+        train, target, test_size=0.2, random_state=0)
+    train_dataset = cb.Pool(X_train, np.log(y_train), cat_features=CAT_COL)
+    val_dataset = cb.Pool(X_val, np.log(y_val), cat_features=CAT_COL)
+    model.select_features(train_dataset, eval_set=val_dataset, steps=5,
+                          features_for_select=train_dataset.get_feature_names(),  # noqa
+                          num_features_to_select=422)
     return model
-def predict(model:cb.core.CatBoostRegressor,path:str):
+
+
+def predict(model: cb.core.CatBoostRegressor, path: str):
     test = pd.read_csv(path)
     for col in CAT_COL:
-        test[col].fillna('',inplace=True)
-    test.drop(['ID','路名', '橫坐標', '縱坐標', '備註'],axis=1,inplace=True)
-    inference = cb.Pool(test,cat_features=CAT_COL) 
+        test[col].fillna('', inplace=True)
+    test.drop(['ID', '路名', '橫坐標', '縱坐標', '備註'], axis=1, inplace=True)
+    inference = cb.Pool(test, cat_features=CAT_COL)
     pred = model.predict(inference)
     pred = np.e**pred
     sub = pd.read_csv('./data/public_private_submission_template.csv')
     sub['predicted_price'] = pred
-    sub.to_csv('./submission.csv',index=False)
+    sub.to_csv('./submission.csv', index=False)
+
 
 def main():
     model = cb.CatBoostRegressor(loss_function="RMSE",
-                            od_type='Iter', od_wait=2000,
-                            verbose=300,depth=8,iterations=10000,
-                            grow_policy='Depthwise',
-                            rsm=0.5,learning_rate=0.01,l2_leaf_reg=2,random_strength=5,eval_metric=mapeMetric(),random_seed=5)
-    model = training(model,'./final_train_processed.csv')
-    predict(model,'final_test_processed.csv')
+                                 od_type='Iter', od_wait=2000,
+                                 verbose=300, depth=8, iterations=10000,
+                                 grow_policy='Depthwise',
+                                 rsm=0.5, learning_rate=0.01, l2_leaf_reg=2,
+                                 random_strength=5, eval_metric=mapeMetric(),
+                                 random_seed=5)
+    model = training(model, './final_train_processed.csv')
+    predict(model, 'final_test_processed.csv')
+
 
 if __name__ == "__main__":
     main()
